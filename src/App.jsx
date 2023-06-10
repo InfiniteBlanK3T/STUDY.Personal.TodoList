e7f3b51049a3f0a53a1074cb67dda982c6309c1f
Thomas <vo.thomas.11647@outlook.com>
2023-06-10 17:14:14 +1000

    New changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,42 @@
+import { useState } from "react";
 import "./style.css";
 
 export default function App() {
-  return "Hi"
+  const [newItem, setNewItem] = useState("")
+  
+
+  return (
+   <>
+    <form className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input 
+        value={newItem}
+        placeholder="Put something here" 
+        onChange={e => setNewItem(e.target.value)} 
+        type="text" 
+        id="item" 
+        />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+    <h1 className="header">Todo List</h1>
+    <ul className="list">
+      <li>
+        <label>
+          <input type="checkbox" />
+          Item 1
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+      <li>
+        <label>
+          <input type="checkbox" />
+          Item 2
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+    </ul>
+   </> 
+  )
 }
